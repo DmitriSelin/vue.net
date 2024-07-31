0ec65cdedf68e945cdb0997313964974582b04b1
selindimitri@gmail.com <selindimitri@gmail.com>
2024-07-31 21:19:49 +0300

    Add constants

---
+++ Api/Common/Constants/CSharpConstants.cs
@@ -0,0 +1,10 @@
+namespace Api.Common.Constants;
+
+public static class CSharpConstants
+{
+    public const string OpenTag = "<cs>";
+
+    public const string CloseTag = "</cs>";
+
+    public const string CodeNamespace = "Vue.NET";
+}
---
+++ Api/Common/Constants/VueConstants.cs
@@ -0,0 +1,6 @@
+namespace Api.Common.Constants;
+
+internal static class VueConstants
+{
+    public const string FileExtension = ".vue";
+}
