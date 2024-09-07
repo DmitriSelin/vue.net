134d8251d2b89572bea6f3b04f59347d96510509
selindimitri@gmail.com <selindimitri@gmail.com>
2024-09-07 22:29:44 +0300

    Rewrite roslyn compile logic

--- Api/Templates/Evaluator/Evaluator.cs
+++ Api/Templates/Evaluator/Evaluator.cs
@@ -1,8 +1,11 @@
 using System.Text;
 using Api.Common.Constants;
-using System.CodeDom;
-using System.CodeDom.Compiler;
-using System.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis;
+using System.Reflection;
+using System.Runtime;
+using Microsoft.CodeAnalysis.Emit;
+using System.Runtime.Loader;
 
 namespace Vue.NET;
 
@@ -43,113 +46,61 @@ public sealed class Evaluator
             }
 
             string code = codeBuilder.ToString();
-            CodeCompileUnit compileUnit = BuildGraph(code);
-            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
-            GenerateCode(provider, compileUnit);
+            code = @"
+            using System;
 
-            string sourceFile;
-            if (provider.FileExtension[0] == '.')
+            namespace RoslynCompileSample
             {
-                sourceFile = "TestGraph" + provider.FileExtension;
+                public class Writer
+                {
+                    public void Write(string message)
+                    {
+                        Console.WriteLine($""you said '{message}!'"");
+                    }
+                }
+            }";
+
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
+            string assemblyName = Path.GetRandomFileName();
+
+            string[] refPaths =
+                [
+                typeof(object).GetTypeInfo().Assembly.Location,
+                typeof(Console).GetTypeInfo().Assembly.Location,
+                Path.Combine(Path.GetDirectoryName(typeof(GCSettings).GetTypeInfo().Assembly.Location)!, "System.Runtime.dll")
+                ];
+            MetadataReference[] references = refPaths.Select(r => MetadataReference.CreateFromFile(r)).ToArray();
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: [syntaxTree],
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+            using var ms = new MemoryStream();
+            EmitResult result = compilation.Emit(ms);
+
+            if (!result.Success)
+            {
+                IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                    diagnostic.IsWarningAsError ||
+                    diagnostic.Severity == DiagnosticSeverity.Error);
+
+                foreach (Diagnostic diagnostic in failures)
+                {
+                    Console.Error.WriteLine("\t{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                }
             }
             else
             {
-                sourceFile = "TestGraph." + provider.FileExtension;
-            }
+                ms.Seek(0, SeekOrigin.Begin);
 
-            CompilerResults cr = CompileCode(provider, sourceFile, "TestGraph.exe");
-
-            if (cr.Errors.Count > 0)
-            {
-                //TODO: Create compile error handling
-            }
-            else
-            {
-                Process.Start("TestGraph.exe");
+                Assembly assembly = AssemblyLoadContext.Default.LoadFromStream(ms);
+                Type? type = assembly.GetType("RoslynCompileSample.Writer");
+                var instance = assembly.CreateInstance("RoslynCompileSample.Writer");
+                MethodInfo? meth = type?.GetMember("Write").First() as MethodInfo;
+                meth?.Invoke(instance, [ "set" ]);
             }
         }
-    }
-
-    public static CodeCompileUnit BuildGraph(string code)
-    {
-        CodeCompileUnit compileUnit = new();
-
-        CodeNamespace codeNamespace = new(CSharpConstants.CodeNamespace);
-        compileUnit.Namespaces.Add(codeNamespace);
-        CodeNamespaceImport[] namespaces = GetNameSpaces(code);
-        CodeNamespaceImport[] defaultNamespaces = GetDefaultNameSpaces();
-        codeNamespace.Imports.AddRange(defaultNamespaces);
-        codeNamespace.Imports.AddRange(namespaces);
-
-        CodeTypeDeclaration currentClass = new("Class1");
-        codeNamespace.Types.Add(currentClass);
-
-        CodeEntryPointMethod entryPoint = new();
-        currentClass.Members.Add(entryPoint);
-
-        return compileUnit;
-    }
-
-    private static CodeNamespaceImport[] GetDefaultNameSpaces()
-    {
-        string[] defaultNameSpaces =
-        [
-            "System", "System.Collections.Generic",
-            "System.Linq", "System.Text",
-            "System.Threading.Tasks"
-        ];
-
-        return defaultNameSpaces
-            .Select(x => new CodeNamespaceImport(x))
-            .ToArray();
-    }
-
-    private static CodeNamespaceImport[] GetNameSpaces(string code)
-    {
-        int startSymbolIndex = code.IndexOf("using ");
-        int endSymbolIndex = code.IndexOf(";");
-        List<CodeNamespaceImport> imports = new();
-
-        if (startSymbolIndex >= 0 && endSymbolIndex >= 0)
-        {
-            string currentNamespace = code.Substring(startSymbolIndex + 1, endSymbolIndex - startSymbolIndex - 1);
-            string fullNamespace = code[startSymbolIndex..endSymbolIndex];
-            imports.Add(new CodeNamespaceImport(currentNamespace));
-            return GetNameSpaces(code.Replace(fullNamespace, string.Empty));
-        }
-        else
-        {
-            return [..imports];
-        }
-    }
-
-    public static void GenerateCode(CodeDomProvider provider, CodeCompileUnit codeCompile)
-    {
-        string sourceFile;
-
-        if (provider.FileExtension[0] == '.')
-        {
-            sourceFile = "TestGraph" + provider.FileExtension;
-        }
-        else
-        {
-            sourceFile = "TestGraph." + provider.FileExtension;
-        }
-
-        IndentedTextWriter tw = new(new StreamWriter(sourceFile, false), "    ");
-        provider.GenerateCodeFromCompileUnit(codeCompile, tw, new CodeGeneratorOptions());
-        tw.Close();
-    }
-
-    public static CompilerResults CompileCode(CodeDomProvider provider, string sourceFile, string exeFile)
-    {
-        string[] referenceAssemblies = ["System.dll"];
-        CompilerParameters cp = new(referenceAssemblies, exeFile, false)
-        {
-            GenerateExecutable = true
-        };
-
-        CompilerResults cr = provider.CompileAssemblyFromFile(cp, sourceFile);
-        return cr;
     }
 }
