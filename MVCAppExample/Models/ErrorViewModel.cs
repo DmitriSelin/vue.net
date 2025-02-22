1316b0c306bd241b9bced453f3c26a4f6299d075
selindimitri@gmail.com <selindimitri@gmail.com>
2025-02-22 15:54:01 +0300

    Add empty MVC project

---
+++ MVCAppExample/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVCAppExample.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
