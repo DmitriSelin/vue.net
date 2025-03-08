7cfe0ba7288f0786b0cdb7ed96b5e173ca79c2ca
selindimitri@gmail.com <selindimitri@gmail.com>
2025-03-08 15:11:45 +0300

    Remove default code (bootstrap, jquery and other)

---
+++ DocumentationApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DocumentationApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
--- MVCAppExample/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace MVCAppExample.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
