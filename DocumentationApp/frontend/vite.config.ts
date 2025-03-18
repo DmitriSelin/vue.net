577c6df59c766acc4e87dd5e4d8100f0e51a4dec
selindimitri@gmail.com <selindimitri@gmail.com>
2025-03-18 23:24:21 +0300

    Add vite configuration for buiding, proxing

--- DocumentationApp/frontend/vite.config.ts
+++ DocumentationApp/frontend/vite.config.ts
@@ -3,5 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [vue()],
+  build: {
+    outDir: '../wwwroot',
+    emptyOutDir: true
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5155/',
+        changeOrigin: true
+      }
+    }
+  }
 })
