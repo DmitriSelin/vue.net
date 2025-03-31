606a4592400958e85452bb9d34d6aea0c42e90f1
selindimitri@gmail.com <selindimitri@gmail.com>
2025-03-31 22:09:21 +0300

    Modify TahHelper: add logic for loading vue-wrapper.js file in application

--- Vue.NET/Infrastructure/Dependencies/VueTagHelper.cs
+++ Vue.NET/Infrastructure/Dependencies/VueTagHelper.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
@@ -8,13 +9,34 @@ public sealed class VueTagHelper : TagHelper
 {
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
+        if (context.Items.ContainsKey(VueConstants.ComponentsLoaded))
+            return;
+
         var vueScript = new TagBuilder("script");
         vueScript.Attributes.Add("src", "https://unpkg.com/vue@3/dist/vue.global.js");
 
-        var wrapperScript = new TagBuilder("script");
-        wrapperScript.Attributes.Add("src", "~/js/vue-wrapper.js");
-
-        output.PostContent.AppendHtml(wrapperScript);
         output.PostContent.AppendHtml(vueScript);
+
+        Assembly currentAssembly = Assembly.GetExecutingAssembly();
+        string? resourceName = currentAssembly.GetManifestResourceNames()
+            .FirstOrDefault(name => name.EndsWith("vue-wrapper.js"));
+
+        if (resourceName == null)
+            throw new FileNotFoundException("vue-wrapper.js not found in assembly's resources");
+
+        using Stream? stream = currentAssembly.GetManifestResourceStream(resourceName);
+
+        if (stream == null)
+            throw new FileLoadException("vue-wrapper.js is not accessible for caller");
+
+        using StreamReader reader = new(stream);
+        string jsCode = reader.ReadToEnd();
+
+        output.PostContent.AppendHtml($@"
+            <script>
+                {jsCode}
+            </script>");
+
+        context.Items[VueConstants.ComponentsLoaded] = true;
     }
 }
