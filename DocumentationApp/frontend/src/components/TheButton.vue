7e8b5b841fcad96ee246d55787ee13ad879d18da
selindimitri@gmail.com <selindimitri@gmail.com>
2025-06-08 18:19:19 +0300

    Delete css classes

--- DocumentationApp/frontend/src/components/TheButton.vue
+++ DocumentationApp/frontend/src/components/TheButton.vue
@@ -1,24 +1,11 @@
-<script lang="ts" setup>
-
-</script>
-
 <template>
-<button>Test click</button>
+    <button>Test click</button>
 </template>
 
-<style lang="css">
-button {
-    width: 100px;
-    height: 60px;
-    padding: 2px;
-    color: whitesmoke;
-    background-color: rgb(131, 2, 131);
-}
+<script lang="ts" setup>
+console.log('pensil');
+</script>
 
-button:hover {
-    background-color: rgb(200, 2, 200);
-    width: 110px;
-    height: 70px;
-    box-shadow: 2px 2px 2px 1px rgb(0 0 0 / 20%);
-}
+<style lang="css">
+
 </style>
