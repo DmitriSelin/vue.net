5dac0018f379611b2db1249a0ad0512edf6e5760
Selin Dmitry <selindimitri@gmail.com>
2026-09-16 22:31:23 +0300

    Remove unused class: Props.cs

--- src/Vue.NET/Infrastructure/Props.cs
+++
@@ -1,9 +0,0 @@
-namespace Vue.NET;
-
-public sealed class Props(Dictionary<string, string> values)
-{
-    public Dictionary<string, string> Values { get; private set; } = values;
-
-    public static Props DefineProps(params (string, string)[] values)
-        => new(values.ToDictionary(x => x.Item1, x => x.Item2));
-}
